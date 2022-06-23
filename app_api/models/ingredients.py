bdc3c33f44d3835cabfab4eda7f8d905d31ac320
Adam Donaghy <donaghyam@gmail.com>
2022-06-23 15:16:07 -0500

    deploying

--- app_api/models/ingredients.py
+++ app_api/models/ingredients.py
@@ -3,7 +3,7 @@ from django.db import models
 class Ingredients(models.Model):
     
     type = models.CharField(max_length=30)
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
     alpha_acids = models.IntegerField(null=True)
     
     class Meta:
